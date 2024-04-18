88215d2279c462e702a6c70b3ad514eb3aed449a
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2024-04-18 10:23:27 -0700

    Deleted commented out CSS lines 1-600

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ const toggleButton = document.querySelector('.toggle-button');
 const nav = document.querySelector('header nav ul');
 
 toggleButton.addEventListener('click', () => {
-    // console.log("button is working");
+
     nav.classList.toggle('active');
 })
